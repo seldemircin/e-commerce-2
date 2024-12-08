84171bcef60abcc999a4e6cff3f7bef9b6491350
Selahattin Demirçin <selahattindemircinn@gmail.com>
2024-12-09 01:53:20 +0300

    version9

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -9,16 +9,16 @@ const raleway = Raleway({
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-violet-300 to-violet-700  h-[90vh] flex justify-center items-center">
+    <div className="bg-gradient-to-br from-violet-300 to-violet-700  h-[90vh] flex justify-center items-center max-lg:p-4">
       <div
-        className={`lg:w-3/4 lg:h-2/3 lg:bg-slate-800 lg:hadow-2xl lg:rounded-3xl lg:bg-opacity-10 flex justify-evenly items-center flex-col space-y-6 ${raleway.className} max-lg:p-2`}
+        className={`lg:w-3/4 lg:h-2/3 bg-slate-800 shadow-2xl rounded-3xl bg-opacity-10 flex justify-evenly items-center flex-col space-y-6 ${raleway.className} max-lg:p-10 h-3/4`}
       >
         <h1 className="text-white text-5xl font-extrabold max-lg:text-4xl max-lg:text-center animate__animated animate__fadeInDown animate__delay-0.5s">
           Learn and buy at the same time!
         </h1>
         <p className="text-lg text-white">Enjoy learning while shopping.</p>
         <div className="space-x-3">
-          <Button variant={"wfullButton"}> Shopping </Button>
+          <Button variant={"wfullButton"}>Start Shopping </Button>
           <Button variant={"wfullButton"}> Docs </Button>
         </div>
       </div>
